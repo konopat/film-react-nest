746e12bf14db30d45f9cc2e501a0fdf1a6858843
konopat <k0n0pat@yandex.ru>
2025-06-28 17:05:06 +0800

    feat/mongodb: connect mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import * as path from 'node:path';
 
 import { configProvider } from './app.config.provider';
@@ -8,6 +9,8 @@ import { FilmsController } from './films/films.controller';
 import { OrderController } from './order/order.controller';
 import { FilmsService } from './films/films.service';
 import { OrderService } from './order/order.service';
+import { FilmsRepository } from './repository/films.repository';
+import { Film, FilmSchema } from './repository/film.schema';
 
 @Module({
   imports: [
@@ -15,12 +18,14 @@ import { OrderService } from './order/order.service';
       isGlobal: true,
       cache: true,
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/prac'),
+    MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema }]),
     ServeStaticModule.forRoot({
       rootPath: path.join(__dirname, '..', 'public', 'content'),
       serveRoot: '/content',
     }),
   ],
   controllers: [FilmsController, OrderController],
-  providers: [configProvider, FilmsService, OrderService],
+  providers: [configProvider, FilmsService, OrderService, FilmsRepository],
 })
 export class AppModule {}
