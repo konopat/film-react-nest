ac42ce8e0e59f134c2458d594b4f9dd27763819f
konopat <k0n0pat@yandex.ru>
2025-06-28 16:09:33 +0800

    feat/order: add dto

--- backend/src/order/dto/order.dto.ts
+++ backend/src/order/dto/order.dto.ts
@@ -1,1 +1,27 @@
 //TODO реализовать DTO для /orders
+
+export class CreateOrderDto {
+  film: string;
+  session: string;
+  row: number;
+  seat: number;
+}
+
+export class OrderItemDto {
+  film: string;
+  session: string;
+  daytime: string;
+  row: number;
+  seat: number;
+  price: number;
+  id: string;
+}
+
+export class OrderResponseDto {
+  total: number;
+  items: OrderItemDto[];
+}
+
+export class OrderErrorDto {
+  error: string;
+}
