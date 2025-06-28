ee30daa036d53ac9bef47359bde07b1ac3242257
konopat <k0n0pat@yandex.ru>
2025-06-28 16:09:01 +0800

    feat/films: add dto

--- backend/src/films/dto/films.dto.ts
+++ backend/src/films/dto/films.dto.ts
@@ -1,1 +1,33 @@
 //TODO описать DTO для запросов к /films
+
+export class FilmDto {
+  id: string;
+  rating: number;
+  director: string;
+  tags: string[];
+  title: string;
+  about: string;
+  description: string;
+  image: string;
+  cover: string;
+}
+
+export class SessionDto {
+  id: string;
+  daytime: string;
+  hall: string;
+  rows: number;
+  seats: number;
+  price: number;
+  taken: string[];
+}
+
+export class FilmsResponseDto {
+  total: number;
+  items: FilmDto[];
+}
+
+export class ScheduleResponseDto {
+  total: number;
+  items: SessionDto[];
+}
