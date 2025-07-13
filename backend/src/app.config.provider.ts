e4725b7c815fe22bd1fdd65ab9f85094d919399f
konopat <k0n0pat@yandex.ru>
2025-07-13 17:59:36 +0800

    feat/config: update app configuration for PostgreSQL

--- backend/src/app.config.provider.ts
+++ backend/src/app.config.provider.ts
@@ -5,11 +5,17 @@ export const configProvider = {
   provide: 'CONFIG',
   useFactory: (configService: ConfigService): AppConfig => ({
     database: {
-      driver: configService.get<string>('DATABASE_DRIVER', 'mongodb'),
-      url: configService.get<string>(
-        'DATABASE_URL',
-        'mongodb://localhost:27017/practicum',
+      driver: configService.get<string>('DATABASE_DRIVER', 'postgres'),
+      host: configService.get<string>('DATABASE_HOST', 'localhost'),
+      port: configService.get<number>('DATABASE_PORT', 5432),
+      username: configService.get<string>('DATABASE_USERNAME', 'postgres'),
+      password: configService.get<string>('DATABASE_PASSWORD', 'postgres'),
+      database: configService.get<string>(
+        'DATABASE_NAME',
+        'film_react_nest_db',
       ),
+      synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE', false),
+      logging: configService.get<boolean>('DATABASE_LOGGING', false),
     },
   }),
   inject: [ConfigService],
@@ -21,5 +27,11 @@ export interface AppConfig {
 
 export interface AppConfigDatabase {
   driver: string;
-  url: string;
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  synchronize: boolean;
+  logging: boolean;
 }
